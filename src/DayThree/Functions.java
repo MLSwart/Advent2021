57c9bdace5857eda4b66469ef9e1cec417db3d62
MLSwart <2konijsjes>
2021-12-03 20:35:54 +0100

    Tot dusver Diagnostic2. Nog niet af.

---
+++ src/DayThree/Functions.java
@@ -0,0 +1,16 @@
+package DayThree;
+
+public class Functions {
+    /*
+    public boolean conformsToOxygenBitCriterium() {
+        if () {
+            return true;
+        } else return false;
+    }
+
+    */
+
+
+}
+
+
