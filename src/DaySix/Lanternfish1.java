a5608850ba93843b60e302c1225ab55755fb5df7
MLSwart <2konijsjes>
2021-12-07 02:53:21 +0100

    Code dag 6, 1e helft. Dit zou toch eenvoudig moeten zijn zou ik denken... Blijkbaar niet.

---
+++ src/DaySix/Lanternfish1.java
@@ -0,0 +1,155 @@
+package DaySix;
+
+public class Lanternfish1 {
+    public static void main(String[] args) {
+        Lanternfish1 fish = new Lanternfish1();
+        fish.run();
+    }
+
+    void run() {
+        int cat0 = 0;
+        int cat1 = 0;
+        int cat2 = 0;
+        int cat3 = 0;
+        int cat4 = 0;
+        int cat5 = 0;
+        int cat6 = 0;
+        int cat7 = 0;
+        int cat8 = 0;
+        int input[] = {3, 4, 3, 1, 2};
+        int numberOfDays = 18;
+        Lanternfish1 fish2 = new Lanternfish1();
+
+
+        for (int x = 0; x < input.length; x++) {
+            if (input[x] < 5) {
+                if (input[x] == 0) {
+                    cat0++;
+
+                } else if (input[x] == 1) {
+                    cat1++;
+
+                } else if (input[x] == 2) {
+                    cat2++;
+
+                } else if (input[x] == 3) {
+                    cat3++;
+
+                } else if (input[x] == 4) {
+                    cat4++;
+
+                }
+            } else if (input[x] >= 5) {
+                if (input[x] == 5) {
+                    cat5++;
+
+                } else if (input[x] == 6) {
+                    cat6++;
+
+                } else if (input[x] == 7) {
+                    cat7++;
+
+                } else if (input[x] == 8) {
+                    cat8++;
+
+                }
+
+            }
+
+        }
+
+
+        //divideInput(input, cat0, cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8);
+
+        System.out.println(cat0 + " , " + cat1 + " , " + cat2 + " , " + cat3 + " , " + cat4 + " , " + cat5 + " , " + cat6 + " , " + cat7 + " , " + cat8);
+        for(int i=0; i<numberOfDays; i++){
+            fish2.moveFish(cat0, cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8);
+        }
+
+
+
+        //for(int i=0; i<numberOfDays; i++){
+        //    moveFish(cat0, cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8);
+        //}
+
+
+    }
+
+
+
+
+
+    /*    public void divideInput(int[] input, int cat0, int cat1, int cat2, int cat3, int cat4, int cat5, int cat6, int cat7, int cat8) {
+            for (int i = 0; i < input.length; i++) {
+                if (input[i] < 5) {
+                    if (input[i] == 0) {
+                        cat0++;
+
+                    } else if (input[i] == 1) {
+                        cat1++;
+
+                    } else if (input[i] == 2) {
+                        cat2++;
+
+                    } else if (input[i] == 3) {
+                        cat3++;
+
+                    } else if (input[i] == 4) {
+                        cat4++;
+
+                    }
+                } else if (input[i] >= 5) {
+                    if (input[i] == 5) {
+                        cat5++;
+
+                    } else if (input[i] == 6) {
+                        cat6++;
+
+                    } else if (input[i] == 7) {
+                        cat7++;
+
+                    } else if (input[i] == 8) {
+                        cat8++;
+
+                    }
+
+                }
+
+            }
+        }
+
+     */
+
+    public void moveFish(int cat0, int cat1, int cat2, int cat3, int cat4, int cat5, int cat6, int cat7, int cat8) {
+        int catReserve = 0;
+        catReserve = cat0;
+        cat0 = cat1;
+        cat1 = cat2;
+        cat2 = cat3;
+        cat3 = cat4;
+        cat4 = cat5;
+        cat5 = cat6;
+        cat6 = cat7 + catReserve;
+        cat7 = cat8;
+        cat8 = catReserve;
+
+        int numberOfFish = cat0 + cat1 + cat2 + cat3 + cat4 + cat5 + cat6 + cat7 + cat8;
+        System.out.println(numberOfFish);
+
+    }
+
+    public void fishCount(int cat0, int cat1, int cat2, int cat3, int cat4, int cat5, int cat6, int cat7, int cat8) {
+        int numberOfFish = cat0 + cat1 + cat2 + cat3 + cat4 + cat5 + cat6 + cat7 + cat8;
+        System.out.println(numberOfFish);
+
+    }
+}
+
+/*
+Wat ik wil doen:
+-Maak 8 categorieën: cat0 (0 dagen over), cat1 (1 dag over), cat2 enz.
+-Zet de aantallen vissen in de categorie van het aantal dagen.
+-Per dag verhuist de hoeveelheid vissen naar de volgende categorie. Uit cat0 verhuizen de vissen naar cat6.
+-Zoveel vissen als verhuizen vanuit cat0 naar cat6 komen erbij in cat8.
+-Tel de categoriën bij elkaar op.
+ */
