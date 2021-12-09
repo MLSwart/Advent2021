62e33299323a3b81973365f7cec2b68650b210db
MLSwart <2konijsjes>
2021-12-09 01:58:26 +0100

    Code dag 8. Nog niet af.

--- src/DayEight/SevenSegment2.java
+++ src/DayEight/SevenSegment2.java
@@ -1,21 +1,47 @@
 package DayEight;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class SevenSegment2 {
     public static void main(String[] args) {
         SevenSegment2 sevenSegment = new SevenSegment2();
         sevenSegment.getCleanInputKeys(sevenSegment.rawInput);
-        sevenSegment.determineLetterDistribution(sevenSegment.cleanInputKeys);
-        sevenSegment.determineLetterDistribution(sevenSegment.cleanInputKeys);
+        int[] distribution = sevenSegment.findDistribution(sevenSegment.cleanInputKeys[0]);
+        HashMap<Integer, String> findEasy = new HashMap<Integer, String>();
+        findEasy.put(6, "B");
+        findEasy.put(4, "E");
+        findEasy.put(9, "F");
+
+        HashMap<Integer, String> findDifficult = new HashMap<Integer, String>();
+        for (int q = 0; q < 7; q++){
+            if (distribution[q] == 7) {
+                if(){
+                    findDifficult.put(7, "D");
+                }
+                else findDifficult.put(7, "G");}
+        if(distribution[q]==8){
+            if(){
+                findDifficult.put(8, "C");
+            }
+            else findDifficult.put(8, "G");
+        }
+            }
 
 
-        /*
-        Neem inputKeys[1].
-        Zet deze code op alfabetische volgorde in een array.
-        is array.length 2, 3, 4 of 7? enz.
 
-         */
+        for (int x = 0; x < distribution.length; x++) {
+
+            System.out.println(findEasy.get(distribution[x]));
+        }
+
+
+//ik maak een distribution
+        //voor elke lijn in de distribution zet ik er de corresponderende hoofdletter bij
+        //kleine letters: 0=a, 1=b enz.
+        //6, 4 en 9 zijn B, E, F.
+        //
+
 
     }
 
@@ -32,6 +58,12 @@ public class SevenSegment2 {
 
     String[] cleanInputKeys = new String[(rawInput.length) / 2];
     String[] cleanInputCodes = new String[(rawInput.length) / 2];
+    char A = 0;
+    char B = 0;
+    char C = 0;
+    char D = 0;
+    char E = 0;
+    char F = 0;
 
     public void getCleanInputCodes(String[] rawInput) {
         int newPos = 0;
@@ -64,49 +96,78 @@ public class SevenSegment2 {
 
     }
 
-    public void determineLetterDistribution(String[] cleanInput) {
-        for (int i = 0; i < cleanInput.length; i++) {
-            String removeCommas = cleanInput[i].replace(" ", "");
-            String[] individualLetters = removeCommas.split("");
-            Arrays.sort(individualLetters);
-            int[] nums = new int[7];
-            for (int k = 0; k < individualLetters.length; k++) {
+    public int[] findDistribution(String cleanInput) {
+        String removeCommas = cleanInput.replace(" ", "");
+        String[] individualLetters = removeCommas.split("");
+        Arrays.sort(individualLetters);
+        int[] distribution = new int[7];
 
-                if (individualLetters[k] == "a") {
-                    nums[0]++;
-                }
-                if (individualLetters[k] == "b") {
-                    nums[1]++;
-                }
-                if (individualLetters[k] == "c") {
-                    nums[2]++;
-                }
-                if (individualLetters[k] == "d") {
-                    nums[3]++;
-                }
-                if (individualLetters[k] == "e") {
-                    nums[4]++;
-                }
-                if (individualLetters[k] == "f") {
-                    nums[5]++;
-                }
-                if (individualLetters[k] == "g") {
-                    nums[6]++;
-                }
+        for (int k = 0; k < individualLetters.length; k++) {
 
+            if (individualLetters[k].equals("a")) {
+                distribution[0]++;
             }
-            System.out.println("A's : " + nums[0]);
+            if (individualLetters[k].equals("b")) {
+                distribution[1]++;
+            }
+            if (individualLetters[k].equals("c")) {
+                distribution[2]++;
+            }
+            if (individualLetters[k].equals("d")) {
+                distribution[3]++;
+            }
+            if (individualLetters[k].equals("e")) {
+                distribution[4]++;
+            }
+            if (individualLetters[k].equals("f")) {
+                distribution[5]++;
+            }
+            if (individualLetters[k].equals("g")) {
+                distribution[6]++;
+            }
+
         }
 
 
+        return distribution;
     }
+
 
 }
 
+//
+//
+//
+//C = find_segment_by_frequency_in("xxxx", 8) # find in "4"
+//D = find_segment_by_frequency_in("xxxx", 7) # find in "4"
+//A = find_segment_by_frequency_not_in(C, 8)
+//G = find_segment_by_frequency_not_in(D, 8)
+
+// HashMap<String,String> findEasy= new HashMap<String, String>();
+// HashMap<int[],String> findEasy= new HashMap<int[], String>();
+
+//HashMap<Integer[], String> findEasy= new HashMap<Integer[], String>(distribution[0]);
+//        findEasy.put(6, "B");
+//        findEasy.put(4, "E");
+//        findEasy.put(9, "F");
 
 
+//for (int x=0; x< distribution.length; x++){
+//    if(distribution[x][1]==6){
+//        B=[x][0];
 
-
+//# 8a, 6b, 8c, 7d, 4e, 9f, 7g
+//0= ABCEFG
+//1=CF
+//2=ACDEG
+//3=ACDFG
+//4=BCDF
+//5=ABDFG
+//6=ABDEFG
+//7=ACF
+//8=ABCDEFG
+//9=ABCDFG
+// return distribution;
 
 /*
 #7       3 segments
