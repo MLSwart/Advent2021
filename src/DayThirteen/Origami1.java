6d2e415a4c7b59cd0ea063ad0bf3390758c5fb66
MLSwart <2konijsjes>
2021-12-14 03:18:42 +0100

    Begonnen aan dag 13. Kaart genereert nu. Nog niet gevouwen.

---
+++ src/DayThirteen/Origami1.java
@@ -0,0 +1,69 @@
+package DayThirteen;
+
+public class Origami1 {
+    public static void main(String[] args) {
+        Origami1 origami = new Origami1();
+        OriInput oriInput= new OriInput();
+        origami.findMax(oriInput.inputRaw);
+        System.out.println(origami.maximumY);
+        System.out.println(origami.maximumX);
+        origami.generateMapArray(oriInput.inputRaw, origami.findMax(oriInput.inputRaw));
+
+
+
+    }
+
+    int maximumX = 0;
+    int maximumY = 0;
+
+    public int[][] findMax(int[] inputRaw) {
+        int[][] mapArray = new int[maximumX+1][maximumY+1];
+        for (int i = 0; i < inputRaw.length; i = i + 2) {
+            if (inputRaw[i] > maximumY) {
+                maximumY = inputRaw[i];
+
+            }
+        }
+        for (int j = 1; j < inputRaw.length; j = j + 2) {
+            if (inputRaw[j] > maximumX) {
+                maximumX = inputRaw[j];
+            }
+
+        }
+        return mapArray;
+
+    }
+
+
+    public void generateMapArray(int[] inputRaw, int[][] mapArray) {
+        for (int i = 0; i< inputRaw.length-1; i=i+2) { //length= aantal rijen (?)
+            int x=inputRaw[i+1];
+            int y=inputRaw[i];
+            mapArray[x][y]=mapArray[x][y]+1;
+
+        }
+
+    }
+}
+
+/*
+Plan van aanpak:
+-Ik verdeel de input in x- en y- waarden, om een array te maken...
+-waarin een waarde van groter dan 1 betekent een stip.
+-Het array wordt <max X waarde> bij <max Y waarde>. 0,0 is linksboven. Kolommen zijn x-waarden. Rijen zijn y-waarden.
+-Er zijn vouw-instructies. We beginnen voor de eerste helft met de eerste instructie.
+-Als het papier vouwt op de y-as gaan de onderste waarden gespiegeld langs die y-as omhoog.
+-Als het papier vouwt op de x-as gaan de rechterwaarden gespiegeld langs die y-as naar links.
+
+-We willen een functie die het initiële array genereert, met de bijbehorende 1'en op de gegeven coordinaten.
+
+-We willen een functie die een y-as (horizontale) vouw simuleert.
+    -Waarden die op de 1e rij onder de vouw zitten, komen op de 1e rij boven de vouw. Dus rij vouw+1 => vouw-1.
+-We willen een functie die een x-as (verticale) vouw simuleert.
+    -Waarden die op de 1e kolom rechts van de vouw zitten, komen op de 1e rij links van de vouw. Dus kolom vouw+1 => vouw-1.
+
+-We willen een functie die het totale aantal stippen telt. Het maakt niet uit of een stip +1, +2, +3 enz is.
+
+
+
+ */
